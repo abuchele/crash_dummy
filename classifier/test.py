5537b4b25bcdf511cb4e2f998e4aa175f57c18b0
abuchele <anna.buchele@students.olin.edu>
2017-03-06 18:41:56 -0500

    the image classifier works!

--- classifier/test.py
+++ classifier/test.py
@@ -1,34 +1,17 @@
 #opens up a webcam feed so you can then test your classifer in real time
 #using detectMultiScale
-import numpy
+import numpy as np
 import cv2
 
 
-
-
 def detect(img):
-    rectsClean = [[0,0,0,0]]
     cascade = cv2.CascadeClassifier("cascade.xml")
     rects = cascade.detectMultiScale(img, 1.3, 4, cv2.cv.CV_HAAR_SCALE_IMAGE, (20,20))
-    hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-    count = 0
-    for guess in rects:
-        [x1,y1,x2,y2] = guess
-        for x in range (x1,x2):
-            for y in range (y1,y2):
-                [h,s,v] = hsv[x,y]
-                print [h,s,v]
-                if ((h < 30) or (h>300)):
-                    count += 1
-                if count > 100:
-                    rectsClean += guess
-        if count > 100:
-            rectsClean += guess
-        count = 0
-    if len(rectsClean) == 0:
+
+    if len(rects) == 0:
         return [], img
-    #rectsClean[:, 2:] += rectsClean[:, :2]
-    return rectsClean, img
+    rects[:, 2:] += rects[:, :2]
+    return rects, img
 
 def box(rects, img):
     for x1, y1, x2, y2 in rects:
@@ -41,11 +24,31 @@ cap.set(4,300)
 
 while(True):
     ret, img = cap.read()
-    rects, img = detect(img)
+    img_hsv=cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    # lower mask (0-10)
+    lower_red = np.array([0,50,50])
+    upper_red = np.array([10,200,200])
+    mask0 = cv2.inRange(img_hsv, lower_red, upper_red)
+
+    # upper mask (170-180)
+    lower_red = np.array([170,50,50])
+    upper_red = np.array([180,255,255])
+    mask1 = cv2.inRange(img_hsv, lower_red, upper_red)
+
+    # join my masks
+    mask = mask0+mask1
+
+    # set my output img to zero everywhere except my mask
+    output_img = img.copy()
+    output_img[np.where(mask==0)] = 0
+
+    rects, img = detect(output_img)
+
     if rects == [[]]:
         pass
     else:
-        box(rects, img)
-    cv2.imshow("frame", img)
+        box(rects, output_img)
+    cv2.imshow("frame", output_img)
     if(cv2.waitKey(1) & 0xFF == ord('q')):
 	break
