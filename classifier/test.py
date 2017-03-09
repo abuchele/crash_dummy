db01127c7045e11d654c3fd0f1bd169a0dc54a19
abuchele <anna.buchele@students.olin.edu>
2017-03-08 19:20:36 -0500

    fixed the color sensing, it only senses the can now! (no people)

--- classifier/test.py
+++ classifier/test.py
@@ -42,12 +42,12 @@ while(True):
     img_hsv=cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
     # lower mask (0-10)
-    lower_red = np.array([0,50,50])
-    upper_red = np.array([10,200,200])
+    lower_red = np.array([0,160,50])
+    upper_red = np.array([10,255,255])
     mask0 = cv2.inRange(img_hsv, lower_red, upper_red)
 
     # upper mask (170-180)
-    lower_red = np.array([170,50,50])
+    lower_red = np.array([170,160,50])
     upper_red = np.array([180,255,255])
     mask1 = cv2.inRange(img_hsv, lower_red, upper_red)
 
