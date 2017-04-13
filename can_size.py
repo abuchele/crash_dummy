dd58327054b26654ff540a07d7a1f4ebabbe60ed
abuchele <anna.buchele@students.olin.edu>
2017-04-13 15:37:25 -0400

    reversing previous merge

---
+++ can_size.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python
+
+#created by Anna, Max, and ChongSwee in 3/2017
+
+#opens up a webcam feed so you can then test your classifer in real time
+#using detectMultiScale
+import numpy as np
+import cv2
+import rospy
+from std_msgs.msg import Bool
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Int8
+
+
+
+doMask = False # set to True if you want to do the red mask, otherwise False
+
+
+def detect(img):
+    if doMask:
+        cascade = cv2.CascadeClassifier("cascade0.xml") #use the classifier that works with the mask
+    else:
+        cascade = cv2.CascadeClassifier("cascade.xml")
+    rects = cascade.detectMultiScale(img, 1.3, 4, cv2.cv.CV_HAAR_SCALE_IMAGE, (20,20))
+
+    if len(rects) == 0:
+        return [], img
+    rects[:, 2:] += rects[:, :2]
+    return rects, img
+
+def box(rects, img):
+    max_area = 0
+    xbig = 0
+    xf1 = 0
+    xf2 = 0
+    yf1 = 0 
+    yf2 = 0 
+    biggest_rect = []
+    for x1, y1, x2, y2 in rects:
+        if (x2-x1) > xbig :
+            xbig = x2 - x1
+            xf1=x1 
+            xf2=x2
+            yf1=y1
+            yf2=y2
+    for x1, y1, x2, y2 in rects:
+        area = abs(x1-x2) * abs(y1-y2)
+        if abs(x1-x2) * abs(y1-y2) > max_area:
+            max_area = area
+            biggest_rect = [x1, y1, x2, y2]
+
+    cv2.rectangle(img, (x1, y1), (x2, y2), (127, 255, 0), 2)
+    angle = ((xf1 + (xf2-xf1)/2)/320.0) * 63 - 31.5
+    cv2.line(img,((xf1+xf2)/2,0),((xf1+xf2)/2,320),(255,255,255),2)
+    return [biggest_rect, angle]
+
+
+def distance(rect, img):
+    #cans have height of 10.47 cm and width of 5.24 cm
+    #The can is just slightly in front of the claw at 17.78 cm from the camera.
+    height_cm = 10.47 #height of the can
+    height_px = 203.0 #can height in pixels at 17.78 cm (7 inches), tbd experimentally
+    ref_dist = 20.32 #the known distance from camera for calibration
+    px_to_cm = float(height_cm/height_px) #multiply by px to get measurement in cm
+    focal_length = (height_px*ref_dist)/height_cm
+
+    y1 = rect[1]
+    y2 = rect[3]
+    height = abs(y1-y2)
+    distance = (height_cm*focal_length)/height
+    print distance
+    if 18.0 < distance < 22.0 :
+        return [True, distance]
+    return [False, distance]
+
+
+def talker(coke_can,miss_stat):
+    pub = rospy.Publisher('img_rec/cmd_vel', Twist, queue_size=10)
+    pub2 = rospy.Publisher('img_rec/miss_stat', Int8, queue_size=10)
+    rospy.init_node('img_rec', anonymous=True)
+
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        rospy.loginfo(coke_can)
+        pub.publish(coke_can)
+        msg.data = miss_stat;
+        pub2.publish(msg)
+        break
+
+def talker_miss_stat(miss_stat):
+    pub2 = rospy.Publisher('/miss_stat', Int8, queue_size=10)
+    #rospy.init_node('img_rec_distance', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        msg.data = miss_stat;
+        pub2.publish(msg)
+        break
+
+cap = cv2.VideoCapture(1) #1 for webcam
+cap.set(3,400)
+cap.set(4,300)
+twist = Twist()
+miss_stat = 1
+msg = Int8()
+
+while(True):
+
+    ret, img = cap.read()
+
+    if doMask:
+        img_hsv=cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+        # lower mask (0-10)
+        lower_red = np.array([0,160,50])
+        upper_red = np.array([10,255,255])
+        mask0 = cv2.inRange(img_hsv, lower_red, upper_red)
+
+        # upper mask (170-180)
+        lower_red = np.array([170,160,50])
+        upper_red = np.array([180,255,255])
+        mask1 = cv2.inRange(img_hsv, lower_red, upper_red)
+
+        # join masks
+        mask = mask0+mask1
+
+        # set output img to zero everywhere except mask
+        output_img = img.copy()
+        output_img[np.where(mask==0)] = 0
+
+    else:
+        output_img = img.copy()
+
+    rects, img_o = detect(output_img)
+
+    if len(rects) == 0:
+        try:
+            twist.angular.z = 0.0
+            twist.linear.x = 0.0
+            miss_stat = 1
+            talker(twist,miss_stat)
+        except rospy.ROSInterruptException:
+            pass
+        pass
+    else:
+        try:
+            [biggest_rect, angle] = box(rects, img_o)
+            twist.angular.z = (angle/180.0)*100
+            twist.linear.x = 20
+            [action, distance] = distance(biggest_rect, img_o)
+
+            if (action):
+                if ((abs(angle) < 0.1)):
+                    miss_stat = 3
+                else:
+                    miss_stat = 2
+                    twist.linear.x = -20
+                    twist.angular.z = 0
+                talker(twist,miss_stat)
+                rospy.sleep(3.)
+            else:
+                miss_stat = 2
+                talker(twist,miss_stat)
+        except rospy.ROSInterruptException:
+            pass
+
+    cv2.imshow("frame", img_o)
+    k = cv2.waitKey(1) & 0xFF
+    if k == ord('q'):
+	       break
--- find_obst.cpp
+++ find_obst.cpp
@@ -12,7 +12,6 @@
 */
 
 
-
 #include <string>
 #include <iostream>
 #include <vector>
@@ -22,47 +21,96 @@
 #include "geometry_msgs/Twist.h"
 
 #define THRESHOLD 0.3 // THRESHOLD FOR CLEAR
-#define VELOCITY 1
+#define VELOCITY 25
 #define ROBO_WIDTH 0.5 // in meters
 
 sensor_msgs::LaserScan scan;
 sensor_msgs::LaserScan filtered_scan;
 
-// ros::Publisher pub_arb;
 ros::Publisher obstacle_flag;
 ros::Publisher lidar_vel;
 
 geometry_msgs::Twist twist;
+std_msgs::Bool flag;
+float smallest;
+float angle_val;
 
 void getLIDAR(const sensor_msgs::LaserScan lidar_scan)
 ;
 
+
+
 void controlSpeed(const sensor_msgs::LaserScan lidar_scan)
 {
+    float forward_distance = 100;
     // set default speed
-    twist.linear.z = VELOCITY;
+    twist.linear.x = VELOCITY;
     twist.angular.z = 0; // straight
 
     scan = lidar_scan;
-    int number_of_ranges = lidar_scan.ranges.size();
+    int number_of_ranges = (int) lidar_scan.ranges.size();
 
-    //always turn right if middle blocked (< THRESHOLD )
-    if(scan.ranges[number_of_ranges/2]<THRESHOLD) {
-      // turn to the left
-      twist.angular.z = 3.142 / 12;
-      obstacle_flag.publish(false);
-      lidar_vel.publish(twist);
-      ROS_INFO("Obstacle detected at centre, moving left");
+    filtered_scan.ranges = lidar_scan.ranges;
+    filtered_scan.header.frame_id = lidar_scan.header.frame_id;
+    filtered_scan.angle_min = lidar_scan.angle_min;
+    filtered_scan.angle_max = lidar_scan.angle_max;
+    filtered_scan.angle_increment = lidar_scan.angle_increment;
+    filtered_scan.range_max = lidar_scan.range_max;
+    filtered_scan.range_min = lidar_scan.range_min;
+
+    // Remove junk values from scan data (0.0 is out of range or too close to be accurate)
+    for(int i=0; i < sizeof(scan.ranges) / sizeof(scan.ranges[0]); i++)
+    {
+        if(filtered_scan.ranges[i] < 0.1 || filtered_scan.ranges[i] > 3.5)
+        {
+            filtered_scan.ranges[i] = 0.0;
+        }
     }
-    else{
-      ROS_INFO("MOVING STRAIGHT");
-      obstacle_flag.publish(true);
-      lidar_vel.publish(twist);
-
+    // Calculate output array using some portion of scan -> set "forward distance" to the smallest value -> the closest object.
+    for (int i = number_of_ranges/3; i<2*number_of_ranges/3;i++)
+    {
+        if ((forward_distance > filtered_scan.ranges[i]) && (filtered_scan.ranges[i] >= 0.1)) {
+            forward_distance = filtered_scan.ranges[i];
+            smallest = i;
+        }
     }
 
+    if (forward_distance < .2)
+    {
+        // Move backward
+        flag.data=1; //avoiding obstacles
+        twist.angular.z = 0; //go straight backwards
+        twist.linear.x = -20; //back up, slowly
+    }
+
+    else if (forward_distance < 0.7)
+    {
+        flag.data = 1;
+        angle_val = (smallest)/ (number_of_ranges);
+        if (angle_val < 0.5) {
+            //turn left
+            twist.angular.z = -50;
+        }
+        else if (angle_val >= 0.5){
+            //turn right
+            twist.angular.z = 50;
+        }
+
+    }
+    else {
+        // Move forward
+        flag.data=0; //not avoiding obstacles
+        twist.angular.z = 0; //go straight forward
+        twist.linear.x = 30; //drive forward
+        //ROS_INFO_STREAM(cmd_array);
+    }
+    obstacle_flag.publish(flag);
+    lidar_vel.publish(twist);
+
+    forward_distance = 100.0;
     return;
 }
+
 
 int main(int argc, char **argv)
 {
