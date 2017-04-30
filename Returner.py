98401c6b8f0420f70b183f88aa8b02f4b72679bd
Maxmilian626 <maxmilian.wei@students.olin.edu>
2017-04-30 17:17:41 -0400

    simple obstacle avoidance.  Needs input from lidar.

---
+++ Returner.py
@@ -0,0 +1,79 @@
+import rospy
+import numpy as np
+
+from std_msgs.msg import Bool
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Int16
+
+class returner:
+
+    def __init__(self):
+
+        rospy.init_node("returner")
+        self.pub = rospy.publisher("command", int16)
+        rospy.Subscriber("goHome", tuple, self.encoder_callback)
+        rospy.Subscriber("lidar_vel", twist, self.get_back)
+        self.leftChange = 0
+        self.rightChange = 0
+        self.angle = np.pi/2.0;
+        self.position = [0,0] #x, y from starting point in cm
+        self.twist = 0
+        self.is_clear = True
+
+        self.WHEEL_DIAMETER = 10 #cm
+        self.ROBOT_WIDTH = 25 #cm, distance between wheels
+        #Conversion factor for one full rotation of pot reading, to length.
+        self.P2L = (self.WHEEL_DIAMETER*np.pi) /  #circumference/fullrotationreading..
+        self.twist = Twist()
+
+
+
+    def encoder_callback(self, data):
+        try:
+            raw = data.data
+            self.leftChange = raw[0]
+            self.rightChange = raw[1]
+
+    def update_position(self):
+        right_length = self.right_change * self.P2L
+        left_length = self.left_change * self.P2L
+        angleChange = (right_length - left_length)/ (ROBOT_WIDTH)
+        dcenter = (left_length - right_length) / 2.0
+
+        #update position data
+        self.position[0] = dcenter*np.cos(self.angle)
+        self.position[1] = dcenter*np.sin(self.angle)
+        self.angle = self.angle + angleChange
+
+#http://faculty.salina.k-state.edu/tim/robotics_sg/Control/kinematics/odometry.html
+    def get_back(self):
+        #Do a thing where with the lidar, have a range of possible angles (0-180).
+        #Arbiter will will make it so that prioritizing obstacle avoidance,
+        #so of those angles, cannot choose the ones that would result in a collision.
+        #Otherwise if no obstacles, can achieve prime directive.
+
+        #Alternatively, if have a map, then can set temporary waypoints
+        #this would help a lot.
+        ideal_angle = np.arctan(self.position[1] / self.position[0]) + np.pi/2.0
+
+        if self.is_clear == False: #To be implemented
+            #wall_follow #Basically turn around to one angle some and move forward.
+            if obstacle_pos = right or center:
+                self.twist.linear.x = 10
+                self.twist.angular.z = -50
+            elif obstacle_pos = left:
+                self.twist.linear.x = 10
+                self.twist.angular.z = 50
+
+        elif self.is_clear:
+            elif (self.angle - ideal_angle) < (-10.0/360.0)/(2*np.pi)
+                self.twist.linear.x = 10
+                self.twist.angular.z = 50 #right turn.
+            elif (self.angle - ideal_angle) > (10.0/360.0)/(2*np.pi)
+                self.twist.linear.x = 10
+                self.twist.angular.z = -50 #Left turn
+            else:
+                self.twist.linear.x = 20 #forward
+                self.twist.angular.z = 0
+
+        pub.publish(twist)
